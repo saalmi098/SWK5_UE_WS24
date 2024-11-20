2c93e81fd301f6d032fbb334b9c93591964fb67e
Andreas Salminger <andisalminger@hotmail.com>
2024-11-20 10:07:08 +0100

    UE06 - ApiDemo

---
+++ UE06/ApiDemo/ApiDemo/Controllers/TimeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers;
+
+// Controller-based Ansatz
+
+// [controller] uebernimmt "Time" aus Klassenname "TimeController"
+//[Route("api/[controller]")]
+[Route("/time2")]
+[ApiController]
+public class TimeController : ControllerBase
+{
+    // Funktionen hier drinnen, um API-Endpunkte zu beschreiben, nennt man "Actions"
+
+    [HttpGet]
+    public object Get()
+    {
+        // retourniert im Format "application/json" ohne es angeben zu muessen
+        // (da JSON der Default-Serializer in ASP.NET ist)
+        return new { Time = DateTime.UtcNow.ToString("o") };
+    }
+}

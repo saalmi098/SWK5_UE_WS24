a3e1f0645c1c39fac9d6c5b853d11cea61b51c2e
Andreas Salminger <andisalminger@hotmail.com>
2024-11-20 12:11:52 +0100

    UE06 - OrderManagement

---
+++ UE06/OrderManagement/OrderManagement.Api/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Api.Dtos;
+using OrderManagement.Api.Mapping;
+using OrderManagement.Domain;
+using OrderManagement.Logic;
+
+namespace OrderManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    private readonly IOrderManagementLogic logic;
+
+    // Dependency Injection
+    public CustomersController(IOrderManagementLogic orderManagementLogic)
+    {
+        logic = orderManagementLogic ?? throw new ArgumentException(null, nameof(orderManagementLogic));
+    }
+
+    // eine Route darf kein Verb enthalten!!! (wichtig fuer Semesterprojekt)
+    // (z.B. createPerson -> das ist SOAP (=funktionsorientiert))
+    // REST = Ressourcen-Orientiert. Verb ergibt sich aus dem HTTP-Verb (GET, POST, PUT, DELETE ...)
+
+
+    // GET <base-url>/api/customers
+    // oder:
+    // GET <base-url>/api/customers?rating=A (optionaler Parameter / Nullable)
+    // [HttpGet("myFancyRoute")] // GET <base-url>/api/customers/myFancyRoute
+    [HttpGet]
+    public async Task<IEnumerable<CustomerDto>> GetCustomers([FromQuery] Rating? rating)
+    {
+        if (rating is null)
+        {
+            return (await logic.GetCustomersAsync()).Select(c => c.ToCustomerDto());
+        }
+        else
+        {
+            return (await logic.GetCustomersByRatingAsync(rating.Value)).Select(c => c.ToCustomerDto());
+        }
+    }
+
+    // GET <base-url>/api/customers/<GUID>
+    [HttpGet("{customerId}")] // Automatisches Mapping, sofern analog zum Variablen-Namen
+    public async Task<ActionResult<CustomerDto>> GetCustomerById([FromRoute] Guid customerId)
+    {
+        var customer = await logic.GetCustomerAsync(customerId);
+
+        if (customer is null)
+        {
+            // 404 returnen, falls customerId ungueltig (TODO Funktioniert (nur bei mir) noch nicht?)
+            return NotFound(); // erfordert ActionResult<...> im Rueckgabetyp
+        }
+
+        return Ok(customer.ToCustomerDto()); // Syntax "Ok(...)" ist optional
+    }
+}
---
+++ UE06/OrderManagement/OrderManagement.Api/Mapping/OrderMapper.cs
@@ -0,0 +1,18 @@
+﻿using OrderManagement.Api.Dtos;
+using OrderManagement.Domain;
+
+namespace OrderManagement.Api.Mapping;
+
+public static class OrderMapper
+{
+    public static OrderDto ToOrderDto(this Order order)
+    {
+        return new OrderDto
+        {
+            Id = order.Id,
+            Article = order.Article,
+            OrderDate = order.OrderDate,
+            TotalPrice = order.TotalPrice
+        };
+    }
+}
